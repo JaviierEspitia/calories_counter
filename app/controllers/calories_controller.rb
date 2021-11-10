85df9fe2fb79f17fd1e66cb7c7b9f9d402e3617c
Javier Espitia <javi.espitiad@gmail.com>
2021-11-09 20:58:34 -0500

    enter calories version 2

--- app/controllers/calories_controller.rb
+++ app/controllers/calories_controller.rb
@@ -4,16 +4,18 @@ class CaloriesController < ApplicationController
     end
 
     def create
-        date_today = DateTime.now.strftime('%Y-%m-%d')
         date_choose = params[:calorie][:day]
-        if(date_choose > date_today)
+        date_choose = date_choose.to_date
+        puts "Haber la fecha de hoy #{Date.today} haber mi eleccion #{date_choose}"
+        if(date_choose > Date.today)
             flash[:danger] = "Fecha escogida en el futuro"
             redirect_to '/home'
         else
             @calorie = Calorie.create(day: params[:calorie][:day], quantity_calories: params[:calorie][:quantity_calories],
             kind: params[:calorie][:kind], user_id: params[:calorie][:user_id], comment: params[:calorie][:comment])
-            render json: @calorie
             flash[:success] = "registro guardado"
+            redirect_to '/home'
+            
         end
         
         
