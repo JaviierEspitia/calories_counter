46801093ab05659606c4d062025214f94fed7856
Javier Espitia <javi.espitiad@gmail.com>
2021-11-11 20:49:41 -0500

    configures sendgrid

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,5 +7,8 @@ class HomeController < ApplicationController
         @calorierender = Calorie.where(user_id: id_user).order(updated_at: :desc).paginate(page: params[:page])
 
         @calories = Calorie.where(user_id: id_user).group(:kind).group(:day).sum(:quantity_calories)
+    
+        puts "Star: #{params[:start]}"
+        puts "End #{params[:end]}"
     end
 end
